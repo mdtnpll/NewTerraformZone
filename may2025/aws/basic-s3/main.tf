cad90f7ee705da1856a31692709c15f589d677fa
Ravi <mdtnpll@gmail.com>
2025-06-11 16:40:28 +0530

    creating s3 bucket

---
+++ may2025/aws/basic-s3/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+# Create a VPC
+resource "aws_s3_bucket" "ravi20071969" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
